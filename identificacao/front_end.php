dce1f828bf506a36e1572d1a64d6a11c61ce861d
Rodvehman <63985415+Rodvehman@users.noreply.github.com>
2025-11-24 16:41:33 -0300

    Update

--- identificacao/front_end.php
+++ identificacao/front_end.php
@@ -1,53 +1,5 @@
-<div class="titulo">Argumentos & Retorno</div>
+<div class="titulo">Front-end</div>
 
 <?php
-    function obterMensagem(){
-        return 'Seja bem vindo<b>';
-    }
-
-    $m = obterMensagem();
-
-    echo $m;
-
-    echo "<br>";
-
-    var_dump(obterMensagem());
-
-    function obterMensagemComNome($nome){
-        return "<br>Bem vindo, $nome<br>";
-    }
-
-    $nome = "Rodrigo";
-
-    echo obterMensagemComNome($nome);
-    echo obterMensagemComNome('Antônio');
-
-    function soma($a, $b){
-        echo "<br>";
-        return $a + $b;
-    }
-
-    echo soma(4,5);
-    
-    $x = 4;
-    $y = 18;
-    echo soma($x,$y);
-
-    function trocaValor($a, $novoValor){
-        $a = $novoValor;
-    }
-
-    $variavel = 1;
-
-    trocaValor($variavel, 3);
-
-    echo "<br>".$variavel;
-    
-    function trocaValorDeVerdade(&$a, $novoValor){
-        $a = $novoValor;
-    }
-    
-    trocaValorDeVerdade($variavel, 5000);
-    
-    echo "<br>".$variavel;
+    echo "Front-End é a parte visual do site. Entendo sua importância, que é gigantesca, mas não me identifiquei.";
 ?>
