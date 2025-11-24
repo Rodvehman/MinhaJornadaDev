6efb3abba88be5c8eca0ff35981614a42f224928
Rodvehman <63985415+Rodvehman@users.noreply.github.com>
2025-11-24 16:57:59 -0300

    Update o_que_sei_fazer.php

--- como_estou/o_que_sei_fazer.php
+++ como_estou/o_que_sei_fazer.php
@@ -1,5 +1,5 @@
 <div class="titulo">O que já sei fazer bem?</div>
 
 <?php
-    
+    echo "De forma geral, tenho facilidade de desenvolver um site. Consegui uma boa lógica para criar as páginas com HTML, CSS, PHP e banco de dados. Preciso estudar mais o Javascript."
 ?>
