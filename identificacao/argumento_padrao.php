4e12c7207c5fcb3425fcf57cd4537f87de5042b3
Rodvehman <63985415+Rodvehman@users.noreply.github.com>
2025-11-24 16:51:29 -0300

    Update argumento_padrao.php

--- identificacao/argumento_padrao.php
+++ identificacao/argumento_padrao.php
@@ -1,27 +1,5 @@
-<div class="titulo">Argumento Padrão</div>
+<div class="titulo">Lógica</div>
 
 <?php
-    function saudacao($nome = 'Senhor(a)', $sobrenome = 'Cliente'){
-        return "Bem vindo, $nome $sobrenome!";
-    }
-
-    echo saudacao();
-    echo '<br>';
-    echo saudacao("Antonio", "Oliveira");
-    echo '<br>';
-
-    function anotarPedido($comida, $bebida = 'Água'){
-        echo "Para comer: $comida<br>";
-        echo "Para beber: $bebida<br>";
-    }
-    
-    anotarPedido('Camarão');
-    anotarPedido('Suchi','Saquê');
-
-    function anotarPedido2($bebida = 'Água', $comida){
-        echo "Para beber: $bebida<br>";
-        echo "Para comer: $comida<br>";
-    }
-
-    anotarPedido2(null, 'Pizza');//Incorreto
+    echo "Sem a lógica, não há como desenvolvermos nada.<br>Para isso, devemos separar em pequenas partes, atribuído as soluções para o problema.<br>Me identifico demais com essa parte."
 ?>
